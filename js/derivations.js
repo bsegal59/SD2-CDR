262390f5a4bc12d2f2b76f4fbd162642b58fd4ac
Brandon Segal <tue92604@temple.edu>
2017-03-01 12:43:30 -0500

    adding code listing

---
+++ js/derivations.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#button1').click(function() {
+        $('#derivation1').toggle('slow');
+    });
+})
+
+$(document).ready(function() {
+    $('#button2').click(function() {
+        $('#derivation2').toggle('slow');
+    });
+})
+
+$(document).ready(function() {
+    $('#button3').click(function() {
+        $('#derivation3').toggle('slow');
+    });
+})
